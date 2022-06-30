049293cfde1812be8d8e26f230a48ad56556ef7d
panda0980 <103883208+panda0980@users.noreply.github.com>
2022-07-01 02:21:56 +0530

    Update __main__.py

--- ub/__main__.py
+++ ub/__main__.py
@@ -1,9 +1,20 @@
-from ub import UBbot
+from pyrogram import idle, Client, filters
+from pyrogram.types import Message
+from ub import UBbot, LOGGER
+from config import *
+from ub import *
+import os
 
 
 
-print("UB started")
+
+
+
+print("PyroGod Started \nPowered By @Perry_XD\n(C) 2021 Paramedra Singh")
+
+
+
 
 if __name__ == "__main__":
-    
-    UBbot.run()
+  UBbot.run()
+  
