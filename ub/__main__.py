65a37e9951e7fb86f0b876cd2659bf85ee696866
panda0980 <103883208+panda0980@users.noreply.github.com>
2022-07-01 02:10:06 +0530

    Update __main__.py

--- ub/__main__.py
+++ ub/__main__.py
@@ -1,4 +1,13 @@
-from ub import UBbot
+import time
+from pyrogram import Client  
+from Config import API_HASH, API_ID, SESSION
+
+HELP = {}
+CMD_HELP = {}
+
+StartTime = time.time()
+
+UBbot = Client(SESSION, api_id=API_ID, api_hash=API_HASH)
 
 
 
