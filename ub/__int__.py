ec99c2bd0f1e448d93acc871ffbabab42c3a684e
panda0980 <103883208+panda0980@users.noreply.github.com>
2022-07-01 02:15:29 +0530

    Update __int__.py

--- ub/__int__.py
+++ ub/__int__.py
@@ -1,12 +1,24 @@
 import time
 from pyrogram import Client  
 from Config import API_HASH, API_ID, SESSION
+import logging
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+)
+
+LOGGER = logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
 HELP = {}
 CMD_HELP = {}
 
 StartTime = time.time()
 
-UBbot = Client(SESSION, api_id=API_ID, api_hash=API_HASH)
+API_ID = API_ID
+API_HASH = API_HASH
+PY_SESSION = SESSION
+
+UBbot = Client(PY_SESSION, api_id=API_ID, api_hash=API_HASH)
 
 
