4e7dba7943641f3d6ac53e58c3c26cd168c50348
panda0980 <103883208+panda0980@users.noreply.github.com>
2022-07-03 22:57:27 +0530

    Update __main__.py

--- ub/__main__.py
+++ ub/__main__.py
@@ -1,11 +1,14 @@
-
-import asyncio
-import os
-from pyrogram import idle, Client, filters
+from pyrogram import Client, filters
 from pyrogram.types import Message
 from ub import UBbot
-from ub import *
 
+
+botcommand = "."
+
+@UBbot.on_message(filters.command("alive", botcommand))
+async def start_message(Client, message:Message):
+    await Client.send_message(message.chat.id, "UB-userbot succefully started")
+   
 
 
 print("UB started")
