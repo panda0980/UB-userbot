488ad6dc63c907b3bfe36b2207abff7ab1a29468
panda0980 <103883208+panda0980@users.noreply.github.com>
2022-07-03 20:45:31 +0530

    Add files via upload

---
+++ ub/helper/database.py
@@ -0,0 +1,43 @@
+"""
+from motor.motor_asyncio import AsyncIOMotorClient
+
+mongodb = AsyncIOMotorClient()
+UBbot_db = mongodb["UBbotd"]
+"""
+import pymongo
+from ub.Config import MONGO_URI
+
+UB_db = pymongo.MongoClient(MONGO_URI)
+
+collection = UB_db["UB"]["pm_guard"]
+
+#add users in APPROVED_USERS list
+async def add_approved(user_id):
+    user =int(user_id)
+    user_check = await collection.find_one({"_id": "APPROVED_USERS"})
+    if user_check:
+        await collection.update_one({"_id":"APPROVED_USERS"},{"$push":{"user_id":user}})
+    else:
+        await collection.insert_one({"_id":"APPROVED_USERS", "user_id":[user]})
+
+#remove users from APPROVED_USERS list
+async def remove_approved(user_id):
+    user = int(user_id)
+    user_check = await collection.find_one({"_id":"APPROVED_USERS"})
+    if user_check:
+        await collection.update_one({"_id":"APPROVED_USERS"},{"$pull":{"user_id":user}})
+    else:
+        return None
+
+#check users in APPROVED_USERS list
+async def check_approved (user_id):
+    user = int(user_id)
+    approved_user = await collection.find_one({"_id": "APPROVED_USERS"})
+    if approved_user:
+        user_list = [users for users in approved_user.get("user_id")]
+        if user in user_list:
+            return True
+        else:
+            return False
+    else:
+        return False
