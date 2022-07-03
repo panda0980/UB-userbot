b93e4ba692b617e5c871a872934e8fba8ac510cc
panda0980 <103883208+panda0980@users.noreply.github.com>
2022-07-03 22:58:29 +0530

    Update pm guard.py

--- ub/pm guard.py
+++ ub/pm guard.py
@@ -2,7 +2,7 @@ import os
 from pyrogram import filters
 from pyrogram.types import Message
 from ub import UBbot
-from database import add_approved, remove_approved, check_approved
+from ub.helper.database import add_approved, remove_approved, check_approved
 
 PMPERMIT_MESSAGE = ("""
     This is PM Security of {} 🛑
